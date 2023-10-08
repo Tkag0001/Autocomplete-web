f5b6b6cb64ff623f7fef5ee401025c71132aade1
Trinh Huynh Thinh Khang <trinhhuynhthinhkhang@gmail.com>
2023-10-08 21:23:05 +0700

    add chart to web

--- autocomplete.js
+++ autocomplete.js
@@ -1,9 +1,14 @@
 var listMonHoc = [];
 var listKhoa = [];
 var listGiangVien = [];
+var listNhomLop = [];
+var listHocKi = [];
 var inputBox_tmp;
+var dataNhomLop;
+// var dataDiem;
 const resultsBox = $(".result-box")
 
+var result_filter = []
 //Get data list of monHoc
 fetch("https://long-lime-iguana-yoke.cyclic.cloud/monHoc")
     .then(res => {
@@ -43,6 +48,24 @@ fetch("https://long-lime-iguana-yoke.cyclic.cloud/giangvien")
     })
     .catch(error => console.log(error));
 
+//Get data list nhom lop and hocKi
+fetch("https://long-lime-iguana-yoke.cyclic.cloud/nhomLop")
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+        dataNhomLop = data;
+        data.forEach(element => {
+            if (!listNhomLop.includes(element["maNhomLop"])) {
+                listNhomLop.push(element["maNhomLop"])
+            }
+            if (!listHocKi.includes(element["hk"])) {
+                listHocKi.push(element["hk"])
+            }
+        });
+    })
+    .catch(error => console.log(error));
 
 function autoComplete(inputBox, availableKeywords) {
     inputBox_tmp = inputBox;
@@ -57,9 +80,9 @@ function autoComplete(inputBox, availableKeywords) {
     // console.log(availableKeywords)
     display(result);
 
-    if(inputBox.val()==""){
+    if (inputBox.val() == "") {
         resultsBox.html("");
-        resultsBox.css("height","0px");
+        resultsBox.css("height", "0px");
     }
 
 }
@@ -91,7 +114,56 @@ function display(result) {
 function selectInput(list) {
     inputBox_tmp.val(list.innerHTML);
     resultsBox.html("");
-    resultsBox.css("height","0px");
+    resultsBox.css("height", "0px");
+}
+
+function alert_noResult() {
+    alert("No reusult!")
+}
+
+function addAtribute(filter, listData, result_filter, n) {
+    if (listData.includes(filter.val())) {
+        result_filter.push(filter.val().substring(0, n));
+    }
+    else {
+        result_filter.push("No_result");
+    }
+}
+
+//Get data list diem
+function getDiem(str) {
+    let result = [];
+    fetch(str)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            // console.log(data)
+            // console.log(data["diem"])
+            keys = Object.keys(data["diem"]);
+            for (var i = 1; i < 12; i++) {
+                console.log(data["diem"][keys[i]])
+                result.push(data["diem"][keys[i]]);
+            }
+
+            // keys.forEach(k => result.push(data["diem"][k]))
+            // console.log(result)
+        })
+        .catch(error => console.log(error));
+    // console.log(result)
+    return result;
+}
+
+function drawChartBar(ob,xArr, yArr, title) {
+    let data = [{
+        x: xArr,
+        y: yArr,
+        type: "bar"
+    }];
+    console.log(data)
+    let layout = {title: title };
+    console.log(layout)
+    Plotly.newPlot(ob, data, layout);
 }
 
 $(document).ready(function () {
@@ -104,5 +176,45 @@ $(document).ready(function () {
     })
     $("#giangVien").keyup(function () {
         autoComplete($(this), listGiangVien);
+    })
+    $("#nhomLop").keyup(function () {
+        autoComplete($(this), listNhomLop);
+    })
+    $("#hocKi").keyup(function () {
+        autoComplete($(this), listHocKi);
+    })
+    $("#search_bt").click(function () {
+        result_filter = [];
+        addAtribute($("#monHoc"), listMonHoc, result_filter, 8);
+        addAtribute($("#giangVien"), listGiangVien, result_filter, 5);
+        addAtribute($("#nhomLop"), listNhomLop, result_filter, 6);
+        addAtribute($("#hocKi"), listHocKi, result_filter, 5)
+
+        console.log(result_filter);
+
+        let strFilter = result_filter.join('_');
+        console.log(strFilter);
+        try {
+            diem = getDiem("https://long-lime-iguana-yoke.cyclic.cloud/nhomLop/" + strFilter);
+            console.log(diem)
+
+            xArr = []
+            yArr = diem
+            for (var i = 0; i <= 10; i++) {
+                xArr.push(i);
+            }
+
+            // data = [{
+            //     x:xArr,
+            //     y:yArr,
+            //     type:"bar"
+            //   }];
+
+            drawChartBar("chart",xArr,yArr,strFilter);
+
+        }
+        catch (error) {
+            console.log(error)
+        }
     })
 });
